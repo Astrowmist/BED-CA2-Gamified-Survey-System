3f3f0272023c5a3f03ea3f820aa35da6e1c38c30
Astrowmist <kmrowhith2006@gmail.com>
2025-02-26 22:30:08 +0800

    Added favicon

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const mainRoutes = require('./routes/mainRoutes.js');
 
-const PORT = 3001;
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
